ab2e6a576f8d7c65cf0861415188955d92253921
binhhx96 <hoangbinhnt1996@gmail.com>
2021-08-20 20:29:43 +0700

    update design

--- src/components/ChatRoom/ChatWindow.js
+++ src/components/ChatRoom/ChatWindow.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
-import {Button, Avatar, Tooltip} from 'antd';
+import {Button, Avatar, Tooltip, Input, Form} from 'antd';
 import {UserAddOutlined} from '@ant-design/icons';
+import Message from './Message';
 
 const HeaderStyled = styled.div`
     display: flex;
@@ -33,12 +34,42 @@ const ButtonGroupStyled = styled.div`
     display: flex;
     align-itemt: center;  
 `;
-const ContentStyled = styled.div``;
+
+const WapperStyled = styled.div`
+    height: 100vh;
+`;
+
+const ContentStyled = styled.div`
+    height: calc(100% - 56px);
+    display: flex;
+    flex-direction: column;
+    padding: 11px;
+    justify-content: flex-end;
+`;
+
+const FormStyled = styled(Form)`
+    display: flex;
+    justify-content: space-between;
+    align-item: center;
+    padding: 2px 2px 2px 0;
+    border: 1px solid rgb(230,230,230);
+    border-radius: 2px;
+
+    .ant-form-item {
+        flex: 1;
+        margin-bottom: 0;
+    }
+`;
+
+const MessageListStyled = styled.div`
+    max-heigh: 100%:
+    over-flow-y: auto;
+`;
 
 //type buton = text de khong co border ben ngoai
 export default function ChatWindow() {
     return (
-        <div>
+        <WapperStyled>
             <HeaderStyled>
                 <div className="header__info">
                     <p className="header__title">Room 1</p>
@@ -60,8 +91,19 @@ export default function ChatWindow() {
                 </ButtonGroupStyled>
             </HeaderStyled>
             <ContentStyled>
-                
+                <MessageListStyled>
+                    <Message text="test" photoURL={null} displayName="Binh" createdAt={12313123} />
+                    <Message text="test" photoURL={null} displayName="Binh" createdAt={12313123} />
+                    <Message text="test" photoURL={null} displayName="Binh" createdAt={12313123} />
+                    <Message text="test" photoURL={null} displayName="Binh" createdAt={12313123} />
+                </MessageListStyled>
+                <FormStyled>
+                    <Form.Item>
+                        <Input bordered={false} autoComplete="off" />
+                    </Form.Item>
+                   <Button type="primary">Gửi</Button>    
+                </FormStyled>
             </ContentStyled>
-        </div>
+        </WapperStyled>
     )
 }
